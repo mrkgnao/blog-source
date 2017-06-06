b11f3aedff228c0068b1b160243245c90a02820b
Soham Chowdhury <chow.soham@gmail.com>
2017-06-06 15:12:46 +0530

    Add helper functions to prelude

--- app/Protolede.hs
+++ app/Protolede.hs
@@ -1,3 +1,25 @@
-module Protolede (module X) where
+module Protolede
+  ( module X
+  , String
+  -- * utilities
+  , length
+  , tshow
+  , stringify
+  ) where
 
-import           Protolude as X
+import           Protolude as X hiding (length)
+import qualified Protolude
+
+import           Data.Text (Text)
+import qualified Data.Text as T
+
+import           GHC.Base  (String)
+
+length :: (Num a, Foldable t) => t b -> a
+length = fromIntegral . Protolude.length
+
+tshow :: Show a => a -> Text
+tshow = T.pack . show
+
+stringify :: (Text -> Text) -> String -> String
+stringify f = T.unpack . f . T.pack
